e79d5b0ce4960435089e1ff0a3bc89476437d4af
Samuel Valdemar Grange <SDU\sagra16@imada-106332.adm.c.sdu.dk>
2019-02-05 15:39:56 +0100

    Fixed build sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cmake CMakeLists.txt -B ./build
-cd build
+cmake CMakeLists.txt
 make all
+rm Makefile
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm CMakeFiles -r
 ./tests
-cd ..
+
--- tests/main.c
+++ tests/main.c
@@ -2,14 +2,16 @@
 // Created by valde on 2/4/19.
 //
 
-#include <setjmp.h>
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
 #include "symbol/symbol_tests.h"
 
+int main() {
+
+	symbol_tests();
+
+	return 0;
+}
+
+/*
 static void do_segv()
 {
     symbol_tests();
@@ -32,7 +34,7 @@ int main()
     sa.sa_flags     = SA_NODEFER;
     sa.sa_sigaction = handler;
 
-    sigaction(SIGSEGV, &sa, NULL); /* ignore whether it works or not */
+    sigaction(SIGSEGV, &sa, NULL); 
 
     if (setjmp(point) == 0) {
         do_segv();
@@ -41,7 +43,7 @@ int main()
         sleep(2);
         fprintf(stderr, "\n FAILURE SEGFAULT FOUND FAILURE\n");
     }
-
     return 0;
 }
 
+*/
