8eb895dfe7a46b9b593875e1a4ec94131998b6dc
Mads Grau Kristensen <madsgrau@gmail.com>
2019-02-18 15:33:30 +0100

    Pretty functions and colors

--- src/scan_parse/pretty.c
+++ src/scan_parse/pretty.c
@@ -4,21 +4,66 @@
 
 char *getTypeName(Type *type);
 
+void prettyKeyword(char *keyword) {
+    printf("\033[1;34m%s\033[0m", keyword);
+}
+
+void prettyType(Type *type) {
+    printf("\033[0;36m%s\033[0m", getTypeName(type));
+}
+
+void prettyArgument(char *id) {
+    printf("\033[0;35m%s\033[0m", id);
+}
+
+void prettyFunctionName(char *id) {
+    printf("\033[0;31m%s\033[0m", id);
+}
+
+void prettyFunction(Function *f) {
+    prettyKeyword("\nfunc");
+    printf(" ");
+    prettyFunctionName(f->head->indentifier);
+    printf("(");
+    VarDelList *dels = f->head->declarationList;
+
+    while (dels != NULL) {
+        prettyArgument(dels->identifier);
+        printf(" : ");
+        prettyType(dels->type);
+        printf(", ");
+        dels = dels->next;
+    }
+
+    printf("\b\b) : ");
+    prettyType(f->head->returnType);
+    printf("\n");
+    prettyBody(f->body);
+    prettyKeyword("end ");
+    prettyFunctionName(f->tail->indentifier);
+}
+
 void prettyDeclaration(Declaration *decl) {
     switch (decl->kind) {
         case varsK:
-            printf("var ");
+            prettyKeyword("var ");
             Declaration *p = decl;
             Declaration *d;
             while (p != NULL) {
                 d = p->val.varsD.var;
-                printf("\t%s : %s\n", d->val.varD.id, getTypeName(d->val.varD.type));
+                printf("%s : ", d->val.varD.id);
+                prettyType(d->val.varD.type);
+                printf(", ");
                 p = p->val.varsD.next;
             }
-            printf(";\n");
+            printf("\b\b;\n");
             break;
         case typeK:
-            printf("type %s : %s;", decl->val.typeD.id, getTypeName(decl->val.typeD.type));
+            prettyKeyword("type");
+            printf(" %s : %s;", decl->val.typeD.id, getTypeName(decl->val.typeD.type));
+            break;
+        case functionK:
+            prettyFunction(decl->val.functionD.function);
             break;
     }
 }
