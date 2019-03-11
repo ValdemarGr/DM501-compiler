654305b85c59dfb20f8a2dbb2e3e41db5734b72f
Abild97 <abild97@hotmail.dk>
2019-03-11 12:30:46 +0100

    Noter fra mødet med Kim

---
+++ src/ASM_code_gen/code_gen.c
@@ -0,0 +1,18 @@
+//
+// Created by Andreas Mortensen on 11-03-2019.
+//
+
+/* @TODO code gen
+ *
+ * Noter og anbefalinger fra Kim:
+ * Start med at lave et proof of concept,
+ * så start fx med nogle få simple ting som:
+ * int, + operator og = operater (måske write)
+ *
+ * Og hvis man kan lave nogle af de svære ting som functioner hvor man kan:
+ * def, kalde, return
+ * måske også lave lokale variabler
+ *
+ * Tips:
+ * behandel main() som en funktion kaldet af ens OS.
+ */
