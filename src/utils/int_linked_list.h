3457ce832e80ff255976ae329d818246a661a50b
Mads Grau Kristensen <madsgrau@gmail.com>
2019-05-01 17:10:27 +0200

    Added missing semi colon

--- src/utils/int_linked_list.h
+++ src/utils/int_linked_list.h
@@ -19,7 +19,7 @@ void appendIntLinkedList(IntLinkedList *list, int value);
 void prependIntLinkedList(IntLinkedList *list, int value);
 IntLinkedListNode *iteratorIntLinkedList(IntLinkedList *list);
 IntLinkedListNode *nextIteratorIntLinkedList(IntLinkedListNode *node);
-int getValueIteratorIntLinkedList(LinkedListNode *node)
+int getValueIteratorIntLinkedList(LinkedListNode *node);
 IntLinkedList *mapIntLinkedList(IntLinkedList *list, int (*transformer)(int));
 
 #endif //HELLO_INT_LINKED_LIST_H
