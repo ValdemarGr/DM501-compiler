af0fc6205f058b291ef822edae3ba24c5c344ccc
ValdemarGr <randomvald0069@gmail.com>
2019-05-03 02:49:30 +0200

    Fixed progs

--- asmsnippets/run.s
+++ asmsnippets/run.s
@@ -1,6 +1,6 @@
 .section .data
 staticLink:
-	.space 16
+	.space 24
 intprint:
 	.asciz "%i\n"
 .section .text
@@ -8,11 +8,12 @@ intprint:
 .extern printf
 # METADATA_BEGIN_BODY_BLOCK
 # VAR a
+# VAR binded
 # METADATA_CREATE_MAIN
 	main:
 	push %rbp
 	movq %rsp, %rbp
-	subq $32, %rsp
+	subq $40, %rsp
 	leaq staticLink, %rax
 	movq %rbp, (%rax)
 # INSTRUCTION_CONST
@@ -27,7 +28,7 @@ intprint:
 		mov $12, %rax
 		syscall
 		push %rax
-		mov $8, %r15
+		mov $16, %r15
 		imul %r14, %r15
 		add %r15, %rax
 		mov %rax, %rdi
@@ -37,7 +38,7 @@ intprint:
 # COMPLEX_MOVE_TEMPORARY_INTO_STACK
 		mov %rcx, -16(%rbp)
 # INSTRUCTION_CONST
-		mov $5, %rdx
+		mov $22, %rdx
 # COMPLEX_MOVE_TEMPORARY_FROM_STACK_IN_SCOPE
 		leaq staticLink, %rax
 		mov 0(%rax), %rax
@@ -46,27 +47,43 @@ intprint:
 		mov $0, %rsi
 # INSTRUCTION_MOVE_TO_OFFSET
 		mov %rdx, (%rbx, %rsi,1)
+# COMPLEX_RIP_LAMBDA_LOAD
+		leaq lambda_0(%rip), %rdi
+# COMPLEX_MOVE_TEMPORARY_FROM_STACK_IN_SCOPE
+		leaq staticLink, %rax
+		mov 0(%rax), %rax
+		mov -16(%rax), %r10
+# INSTRUCTION_CONST
+		mov $8, %r11
+# INSTRUCTION_MOVE_TO_OFFSET
+		mov %rdi, (%r10, %r11,1)
 # COMPLEX_ALLOCATE_END
 		pop %r14
 		pop %r15
 		pop %rax
 		pop %rdi
-# INSTRUCTION_CONST
-		mov $77, %rdi
 # COMPLEX_MOVE_TEMPORARY_FROM_STACK
-		mov -16(%rbp), %r8
+		mov -16(%rbp), %r12
 # INSTRUCTION_CONST
-		mov $0, %r9
-# INSTRUCTION_MOVE_TO_OFFSET
-		mov %rdi, (%r8, %r9,1)
-# COMPLEX_MOVE_TEMPORARY_FROM_STACK
-		mov -16(%rbp), %r10
-# INSTRUCTION_CONST
-		mov $0, %r11
+		mov $8, %r13
 # COMPLEX_DEREFERENCE_POINTER_WITH_OFFSET
-		mov (%r10, %r11,1), %r10
+		mov (%r12, %r13,1), %r12
+# COMPLEX_MOVE_TEMPORARY_INTO_STACK
+		mov %r12, -24(%rbp)
+# COMPLEX_MOVE_TEMPORARY_FROM_STACK
+		mov -16(%rbp), %r14
+# INSTRUCTION_CONST
+		mov $0, %r15
+# COMPLEX_DEREFERENCE_POINTER_WITH_OFFSET
+		mov (%r14, %r15,1), %r14
+# INSTRUCTION_PUSH
+		push %r14
+# COMPLEX_MOVE_TEMPORARY_FROM_STACK
+		mov -24(%rbp), %rax
+# INSTRUCTION_REGISTER_CALL
+		call *%rax
 # INSTRUCTION_WRITE
-		movq %r10, %rsi
+		movq %rax, %rsi
 		movq $intprint, %rdi
 		movq $0, %rax
 		call printf
@@ -74,3 +91,26 @@ intprint:
 mov $60, %rax
 mov $0, %rdi
 syscall
+# INSTRUCTION_FUNCTION_LABEL
+.type lambda_0, @function
+lambda_0:
+push %rbp
+mov %rsp,%rbp
+	subq $24, %rsp
+	leaq staticLink, %r8
+	movq %rbp, 16(%r8)
+# INSTRUCTION_MINUS
+	mov 16(%rbp), %r9
+	mov %r9, -8(%rbp)
+# COMPLEX_MOVE_TEMPORARY_FROM_STACK
+	mov -8(%rbp), %r10
+# INSTRUCTION_CONST
+	mov $2, %r11
+# INSTRUCTION_MUL
+	imul %r10, %r11
+# INSTRUCTION_RETURN
+	mov %r11, %rax
+	mov %rbp,%rsp
+pop %rbp
+ret
+# METADATA_END_BODY_BLOCK
