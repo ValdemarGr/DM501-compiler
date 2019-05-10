828cadfc23f213ac7a39d9613bb617ab1bf360ef
Mads Grau Kristensen <madsgrau@gmail.com>
2019-05-11 01:15:38 +0200

    runAllProgramsThroughCompiler now does as promised

--- runAllProgramsThroughCompiler.sh
+++ runAllProgramsThroughCompiler.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/decl_test.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/functions_only.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/lots_of_nests.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/lambda.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/val.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/declarative_programming.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/class.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/inheritance.kitty
-./cmake-build-debug/src/Compiler -t $(pwd)/kitty/list_monad.kitty
+compiler=./cmake-build-debug/src/compiler
+output_directory=./build-kitty
+directories=(kitty kimtests)
+
+mkdir -p ${output_directory}
+
+number_files=0
+number_errors=0
+
+function compile_file {
+    local filepath="$1"
+    local filename=$(basename -- "$filepath")
+    local filename="${filename%.*}"
+    local subdirectory="$2"
+
+    local output_file="$output_directory/$subdirectory/$filename.s"
+    local error="$(${compiler} ${filepath} 2<&1 > ${output_file})"
+    number_files=$((number_files + 1))
+
+    if [[ -n "$error" ]]; then
+        echo "Error in $filepath"
+        echo -ne "\e[1m\e[31m"
+        echo "$error"
+        echo -ne "\e[0m"
+        echo
+        number_errors=$((number_errors + 1))
+    fi
+}
+
+for dir in ${directories[*]}; do
+    mkdir -p "$output_directory/$dir"
+    for filename in ./${dir}/*.kitty ./${dir}/*.src; do
+        if [[ -f "$filename" ]]; then
+            compile_file ${filename} ${dir}
+        fi
+    done
+done
+
+
+echo "Compiled $number_files files. $number_errors errors"
