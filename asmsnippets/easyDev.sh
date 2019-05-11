fc37759f86c3ee88b4f2b45964bf2562a6c0fcc7
ValdemarGr <randomvald0069@gmail.com>
2019-05-11 15:01:50 +0200

    Args for mem

--- asmsnippets/easyDev.sh
+++ asmsnippets/easyDev.sh
@@ -1,7 +1,7 @@
 cd ../build-files
 rm compiler
 make compiler
-./compiler -mem=30 < ./../kitty/$1.kitty > ./../asmsnippets/run.s
+./compiler -mem=1000 < ./../kitty/$1.kitty > ./../asmsnippets/run.s
 cd ./../asmsnippets
 gcc -g -no-pie -m64 -o out run.s
 objcopy --only-keep-debug out out.debug
